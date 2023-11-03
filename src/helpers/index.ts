ea12fece0ec5d8503c54df7a548314787177797e
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-11-03 07:31:40 +0530

    fix: api url changes

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -8,7 +8,7 @@ const helpers: any = {
   apiURL:
     process.env.NODE_ENV !== "production"
       ? `http://localhost:3000`
-      : `https://textify-alpha.vercel.app`,
+      : `https://textify-ai.vercel.app`,
 };
 
 export { helpers };
